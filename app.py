1d337089f0ecd9d74296d4861ca4387fb13ccafe
ajeevsan <sanjeevmajhi036@gmail.com>
2023-02-25 13:45:15 +0530

    Adding new files

--- app.py
+++ app.py
@@ -3,12 +3,30 @@ from flask import Flask, render_template
 #! creating an object of the flask
 app = Flask(__name__)
 
+sample_dict = [
+    {
+        'id': 1,
+        'name': 'Sanjeev',
+        'age': 24
+    },
+    {
+        'id': 2,
+        'name': 'Subham',
+        'age': 24
+    },
+    {
+        'id': 3,
+        'name': 'Kabir',
+        'age': 24
+    }
+]
+
+
 @app.route("/")
 def hello_world():
-    return render_template('home.html')
+    return render_template('home.html', data=sample_dict)
 
 
 if __name__ == "__main__":
     print("I'm inside under name now.")
-    app.run(host='0.0.0.0', debug=True) 
-
+    app.run(host='0.0.0.0', debug=True)
